d0779d4998e368ed9910ff1d3a44454c7cbd62d9
spruyt <s163017@ap.be>
2026-05-11 11:35:15 +0200

    labs

---
+++ FinalProjectVRExp/Assets/Scripts/LabMixer.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using TMPro; // Dit is nodig om TextMeshPro aan te sturen!
+
+public class ScheikundeMixer : MonoBehaviour
+{
+    [Header("Voortgang")]
+    public bool heeftStofA = false;
+    public bool heeftStofB = false;
+
+
+    [Header("Instellingen")]
+    public float gietHoek = 100f; 
+    public GameObject objectDatVerdwijnt;
+    public GameObject objectDatVerschijnt;
+
+    [Header("In-Game Debugger")]
+    public TextMeshPro debugScherm; // Sleep hier je zwevende tekst in
+    private string laatsteMelding = "Wachten op glazen...";
+    private float actueleKanteling = 0f;
+
+    void Update()
+    {
+        // Als we een debug-scherm hebben gekoppeld, werk deze dan elke frame bij!
+       
+    }
+
+    void OnTriggerStay(Collider anderObject)
+    {
+        // Update de live kanteling-meter voor de debugger
+        actueleKanteling = Vector3.Angle(anderObject.transform.up, Vector3.up);
+
+        if (actueleKanteling >= gietHoek)
+        {
+            CheckFlesje(anderObject);
+        }
+    }
+
+    void OnTriggerExit(Collider anderObject)
+    {
+        // Zet de hoek-meter weer op 0 als we het glas weghalen
+        actueleKanteling = 0f;
+    }
+
+    void CheckFlesje(Collider flesje)
+    {
+        if (flesje.CompareTag("StofA") && !heeftStofA)
+        {
+            heeftStofA = true;
+            GietSucces(flesje.gameObject, "Stof A");
+        }
+        else if (flesje.CompareTag("StofB") && !heeftStofB)
+        {
+            heeftStofB = true;
+            GietSucces(flesje.gameObject, "Stof B");
+        }
+
+    }
+
+    void GietSucces(GameObject flesje, string stofNaam)
+    {
+        laatsteMelding = stofNaam + " is met succes gegoten!";
+        flesje.tag = "Untagged"; 
+        CheckOfMinigameKlaarIs();
+    }
+
+    void CheckOfMinigameKlaarIs()
+    {
+        if (heeftStofA && heeftStofB)
+        {
+            laatsteMelding = "SUCCES! De eindstof is gemaakt!";
+            
+            if (objectDatVerdwijnt != null) objectDatVerdwijnt.SetActive(false);
+            if (objectDatVerschijnt != null) objectDatVerschijnt.SetActive(true);
+        }
+    }
+}
