1185327c0ecf08d9e9844e185d8c00033f6edfe5
Jelle Haast <jelle.haast@gmail.com>
2026-05-27 12:50:33 +0200

    fixed some scripts to work again after minor error

--- FinalProjectVRExp/Assets/Scripts/RotateSateliteDish.cs
+++ FinalProjectVRExp/Assets/Scripts/RotateSateliteDish.cs
@@ -5,7 +5,7 @@ public class RotateObject : MonoBehaviour
     [SerializeField] private float rotationSpeed = 30f;
     [SerializeField] private float targetAngle = 240f;
     [SerializeField] private AudioSource audioSource;
-    //public bool turnOn = false;
+    public bool turnOn = false;
 
     public QuestData Quest;
     public QuestManager manager;
@@ -13,13 +13,13 @@ public class RotateObject : MonoBehaviour
     private float rotated = 0f;
     private bool rotating = false;
 
-    /*public void Update()
+    public void Update()
     {
         if (turnOn)
         {
             StartRotation();
         }
-    }*/
+    }
     public void StartRotation()
     {
         if (!rotating)
